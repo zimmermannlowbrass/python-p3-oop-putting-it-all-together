6669581fb263f464dff67af0d78baac5bb7936c9
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-26 11:16:35 -0500

    completed labwork

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def get_pagecount(self):
+        return self._page_count
+    
+    def set_pagecount(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_pagecount, set_pagecount)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!") 
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, condition='', size=0):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
